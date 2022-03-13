1404fab14f066ad6f5069e68e4975232e1609fb4
Дмитрий Королев <dimkor83@yandex.ru>
2022-03-13 14:33:16 +0400

    modified task

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -2,10 +2,15 @@ public class BonusMilesService {
     public int calculate(int cost) {
 
         int rubmile = 20;
-        int ticket = 20;
+        int ticket = 10;
         int total = cost * ticket;
         boolean isRegistered = true;
 
-        return isRegistered ? total / rubmile : 0;
+        int miles = isRegistered ? total / rubmile : 0;
+//        int limit = 500;
+//        if (miles > limit) {
+//            miles = limit;
+//        }
+        return miles;
     }
 }
